f7574fc3f45589e7c93da757ed282e27eb868b08
Ahmed Khattab <ahmedibraheem93@gmail.com>
2020-11-10 13:26:35 +0200

    [COV-47] Rename the project and make sure that it is working

--- android/app/src/main/java/com/sidebench/covis/MainActivity.java
+++ android/app/src/main/java/com/sidebench/covis/MainActivity.java
@@ -1,4 +1,4 @@
-package com.blink22.reactnativeskeleton;
+package com.sidebench.covis;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "ReactNativeSkeleton";
+        return "Covis";
     }
 
     @Override
