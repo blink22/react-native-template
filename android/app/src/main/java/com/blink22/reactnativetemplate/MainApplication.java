59e187fad055d0faab19abef02793817a63cb1f5
Ahmed Khattab <ahmedibraheem93@gmail.com>
2020-11-10 16:28:30 +0200

    Project setup (#1)
    * [COV-47] Rename the project and make sure that it is working
    
    * [COV-47] Update dependencies
    
    * [COV-47] Update android build version
    
    * [COV-47] Update realease notes
    
    * [COV-47] Add eslint hooks plugin
    
    * Rename the project to template
    
    * Update readME file

--- android/app/src/main/java/com/blink22/reactnativetemplate/MainApplication.java
+++ android/app/src/main/java/com/blink22/reactnativetemplate/MainApplication.java
@@ -1,6 +1,5 @@
-package com.blink22.reactnativeskeleton;
+package com.blink22.reactnativetemplate;
 
-import com.brentvatne.react.ReactVideoPackage;
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
@@ -27,7 +26,6 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-          packages.add(new ReactVideoPackage());
           return packages;
         }
 
